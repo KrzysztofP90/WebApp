2c38ae6d27478c9d7c6152a3dfdc5249a4ff3015
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-01-13 15:17:15 +0100

    Add static content handling.

--- src/main/webapp/java/MyServlet.java
+++ src/main/webapp/java/MyServlet.java
@@ -2,6 +2,7 @@
 
 import org.jtwig.web.servlet.JtwigRenderer;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,16 +20,39 @@ public class MyServlet extends HttpServlet {
                           HttpServletResponse response)
             throws ServletException, IOException {
 
-//        response.getWriter().write("<html><body>It's working!!!</body></html>");
+//        response.getWriter().write("<html><body><h1>It's working!!!</h1></body></html>");
 
         String title = "MyPage";
         String name = "Twigi";
 
+        String cssURL = request.getContextPath() + "resources/styles.css";
+
+        String jsURL = request.getContextPath() + "resources/main.js";
+
+        String logoURL = request.getContextPath() + "resources/logo.png";
+
+        String companyName = "Client's company";
+
 //        request.setAttribute("My Page", "Twigi");
-        renderer.dispatcherFor("/WEB-INF/templates/start.twig").with("title", title).
+        renderer.dispatcherFor("/WEB-INF/templates/start.twig").
+                with("title", title).
+                with("css", cssURL).
+                with("scriptURL", jsURL).
                 with("person", name).
+                with("logoURL", logoURL).
+                with("companyName", companyName).
                 render(request,response);
 
+
+
+
+        /// send css by servlet ???
+
+
+//        RequestDispatcher dispatcher = request.getRequestDispatcher(
+//                "/WEB-INF/stylesheet/styles.css");
+//
+//        dispatcher.include(request, response);
 
 
     }
